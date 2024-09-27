4c52898114634369bd332542490cc0a20e3d912d
parkhyungjun <pakrhyungjun@kico.co.jp>
2024-09-27 15:15:27 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
 
+const customers =[{
+  'id':1,
+  'image': 'https://screenchaser.kico.co.jp/wp-content/uploads/2024/07/ScreenCharser-800x285.png',
+  'name': 'kico1',
+  'birthday': '961111',
+  'gender': 'man',
+  'job': 'college'
+},
+{
+  'id':2,
+  'image': 'https://screenchaser.kico.co.jp/wp-content/uploads/2024/07/ScreenCharser-800x285.png',
+  'name': 'kico2',
+  'birthday': '971111',
+  'gender': 'waman',
+  'job': 'college'
+},
+{
+  'id':3,
+  'image': 'https://screenchaser.kico.co.jp/wp-content/uploads/2024/07/ScreenCharser-800x285.png',
+  'name': 'kico3',
+  'birthday': '981111',
+  'gender': '?',
+  'job': 'college'
+}
+]
 class App extends Component {
   render(){
     return(
-    <div className="gray-background">
-        <img src={logo} alt ="logo" />
-        <h2>Let's develop management system!</h2>
+      <div>
+    {
+      customers.map(c => {
+        return(
+          <Customer 
+            key={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        )
+      })
+    }
     </div>
     );
   }
